23261a0ce68c7060ab1f1fd9124c328a5bc6d18f
dorian dons <dorian.doncevic@gmail.com>
2021-11-11 17:26:29 +0100

    Dodani pathovi za svaki zadatak

--- index.js
+++ index.js
@@ -10,4 +10,16 @@ express()
   .get('/', function(req, res){
     res.send("Hello World 12345");
   })
+  .get('/', function(req, res){
+    res.send("2 Projekt iz WIM2");
+  })
+  .get('/1', function(req, res){
+    res.send("SQL umetanje (SQL Injection)");
+  })
+  .get('/2', function(req, res){
+    res.send(" Loša kontrola pristupa (Broken Access Control)");
+  })
+  .get('/3', function(req, res){
+    res.send("Vanjski XML entiteti (XML External Entity, XXE)");
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
