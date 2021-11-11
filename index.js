1c74d364d8b93e7dfa5095aa6e4bb7cdbd3cff24
dorian dons <dorian.doncevic@gmail.com>
2021-11-11 17:18:49 +0100

    Heroku Test2

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ express()
   .use(express.json())
   .set("view engine", "ejs")
   .get('/', function(req, res){
-    res.send("Hello World 1234");
+    res.send("Hello World 12345");
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
