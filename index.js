202af8fa8632a4cfd25afed2e0f9d3f50d3292e1
dorian dons <dorian.doncevic@gmail.com>
2021-11-14 22:26:20 +0100

    Promjena logike za sigurnost stranice

--- index.js
+++ index.js
@@ -6,6 +6,62 @@ var urlencodedParser = bodyParser.urlencoded({extended: false})
 const PORT = process.env.PORT || 3000
 var pjXML = require('pjxml');
 var parseString = require('xml2js').parseString;
+
+const public = [
+  {
+    "_id": "61902fe6490847406468e661",
+    "index": 0,
+    "guid": "2aef2514-6850-4f55-ac89-3f3d58da4066",
+    "isActive": false,
+    "balance": "$3,346.91",
+    "picture": "http://placehold.it/32x32",
+    "age": 20,
+    "eyeColor": "blue",
+    "name": "Gill Booker",
+    "gender": "male",
+    "company": "COMVOY",
+    "email": "gillbooker@comvoy.com",
+    "phone": "+1 (964) 468-2932",
+    "address": "541 Kingston Avenue, Fresno, Kansas, 564",
+    "about": "Labore ex cupidatat consequat eiusmod in ex proident. Excepteur in cupidatat enim ut qui mollit. Cillum sit aliqua quis aute commodo in anim aliquip sit deserunt. Cillum pariatur pariatur Lorem ex cillum ex amet qui nostrud amet do fugiat cillum. Officia non est elit fugiat Lorem quis duis proident duis excepteur sunt cillum. Ullamco occaecat proident aute sint labore anim eu quis consectetur in est nisi ad.\r\n",
+    "registered": "2014-01-22T03:17:02 -01:00",
+    "latitude": -62.574969,
+    "longitude": -53.114542,
+    "tags": [
+      "dolore",
+      "in",
+      "dolore",
+      "esse",
+      "pariatur",
+      "id",
+      "sunt"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Helga Dominguez"
+      },
+      {
+        "id": 1,
+        "name": "Jenna Kane"
+      },
+      {
+        "id": 2,
+        "name": "Burks Knowles"
+      }
+    ],
+    "greeting": "Hello, Gill Booker! You have 7 unread messages.",
+    "favoriteFruit": "banana"
+  }
+]
+
+const private = [{
+  "title": "Jako bitan JSON, ovo samo admin smije vidit",
+  "user": "admin",
+  "password": "pa$$w0rd"
+}]
+
+var testiranje = true;
  
 express()
 
@@ -14,19 +70,36 @@ express()
 
   .set("view engine", "ejs")
   .get('/', function(req, res){
-    res.send("2 Projekt iz WIM2");
+    res.render("home", {
+      sigurnost: testiranje
+    });
+  })
+  .get('/bac', function(req, res){
+    res.render("bac", {
+      sigurnost: testiranje
+    });
+  })
+  .get('/private', function(req, res){
+    res.send(private);
+  })
+  .post('/public', function(req, res){
+    res.send(public);
   })
   .get("/sql", (req, res, next) => {
     res.render("movies", {
       movies: {},
-      valid: false
+      valid: false,
+      sigurnost: testiranje
     })
   })
   .post('/sql', urlencodedParser, function(req, res){
     var name = req.body['movie_name']
     var security = req.body['security']
 
-    if(security == 'sigurno'){
+    // if(security == 'sigurno'){
+    //  name = name.toLowerCase().replace(/[^a-zA-Z0-9]+/g, " ");
+    //}
+    if(testiranje){
       name = name.toLowerCase().replace(/[^a-zA-Z0-9]+/g, " ");
     }
     console.log(name)
@@ -37,40 +110,45 @@ express()
           console.log(err.message)
           res.render("movies", {
             movies: rows,
-            valid: false
+            valid: false,
+            sigurnost: testiranje
           });
           return
         }
         res.render("movies", {
           movies: rows,
-          valid: true
+          valid: true,
+          sigurnost: testiranje
         });
       });
   })
   .get('/xml', function(req, res){
     res.render("xml", {
-      document: ""
+      document: "",
+      sigurnost: testiranje
     });
   })
   .post('/xml', urlencodedParser, function(req, res){
     var xml = req.body['xml']
     var security = req.body['security']
     
-    if(security == 'sigurno'){
+    if(testiranje){
       parseString(xml, function (err, result) {
         var data = JSON.stringify(result);
         res.render("xml", {
-          document: data
+          document: data,
+          sigurnost: testiranje
         })
     });
     }
     else{
       var doc = pjXML.parse(xml)
       var data = JSON.stringify(doc);
-      console.log(doc)
-      console.log(data)
+      // console.log(doc)
+      // console.log(data)
       res.render("xml", {
-        document: data
+        document: data,
+        sigurnost: testiranje
       });
     }
     
@@ -82,5 +160,9 @@ express()
     var doc = pjXML.parse(xml)
     var doc2 = pjXML.parse(xxe)
     res.send(doc2) // ["content"][3]["content"][0]
+  })
+  .post('/change-security', function(req, res){
+    testiranje = !testiranje;
+    res.send(testiranje);
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
